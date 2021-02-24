6efe857e5ef6be594d874bbb700b3d040599e106
billy david <williamdavid423@hotmail.com>
2021-02-24 14:38:21 -0500

    Add project files.

---
+++ adventure game/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace adventure_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topButtonLabel = new System.Windows.Forms.Label();
+            this.bottomButtonLabel = new System.Windows.Forms.Label();
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.imageBox1 = new System.Windows.Forms.PictureBox();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.mainPictureBox2 = new System.Windows.Forms.PictureBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // topButtonLabel
+            // 
+            this.topButtonLabel.AutoSize = true;
+            this.topButtonLabel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topButtonLabel.Location = new System.Drawing.Point(150, 299);
+            this.topButtonLabel.Name = "topButtonLabel";
+            this.topButtonLabel.Size = new System.Drawing.Size(69, 24);
+            this.topButtonLabel.TabIndex = 7;
+            this.topButtonLabel.Text = "label1";
+            // 
+            // bottomButtonLabel
+            // 
+            this.bottomButtonLabel.AutoSize = true;
+            this.bottomButtonLabel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomButtonLabel.Location = new System.Drawing.Point(150, 384);
+            this.bottomButtonLabel.Name = "bottomButtonLabel";
+            this.bottomButtonLabel.Size = new System.Drawing.Size(69, 24);
+            this.bottomButtonLabel.TabIndex = 9;
+            this.bottomButtonLabel.Text = "label1";
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.mainPictureBox.Location = new System.Drawing.Point(31, 108);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(436, 168);
+            this.mainPictureBox.TabIndex = 11;
+            this.mainPictureBox.TabStop = false;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.imageBox1.Location = new System.Drawing.Point(31, 384);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(60, 54);
+            this.imageBox1.TabIndex = 8;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox
+            // 
+            this.imageBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.imageBox.Location = new System.Drawing.Point(31, 299);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(60, 54);
+            this.imageBox.TabIndex = 6;
+            this.imageBox.TabStop = false;
+            // 
+            // mainPictureBox2
+            // 
+            this.mainPictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.mainPictureBox2.Location = new System.Drawing.Point(154, 108);
+            this.mainPictureBox2.Name = "mainPictureBox2";
+            this.mainPictureBox2.Size = new System.Drawing.Size(179, 168);
+            this.mainPictureBox2.TabIndex = 12;
+            this.mainPictureBox2.TabStop = false;
+            this.mainPictureBox2.Visible = false;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Location = new System.Drawing.Point(31, 13);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(436, 81);
+            this.titleLabel.TabIndex = 13;
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.titleLabel.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 450);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.mainPictureBox2);
+            this.Controls.Add(this.mainPictureBox);
+            this.Controls.Add(this.bottomButtonLabel);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.topButtonLabel);
+            this.Controls.Add(this.imageBox);
+            this.Name = "Form1";
+            this.Text = "   Fortnite Battle Royale";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.Label topButtonLabel;
+        private System.Windows.Forms.PictureBox imageBox1;
+        private System.Windows.Forms.Label bottomButtonLabel;
+        private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.PictureBox mainPictureBox2;
+        private System.Windows.Forms.Label titleLabel;
+    }
+}
+
---
+++ adventure game/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace adventure_game
+{
+    public partial class Form1 : Form
+    {
+        int scene = 0;
+        public Form1()
+        {  InitializeComponent();
+            imageBox.BackgroundImage = Properties.Resources.greenButton;
+            topButtonLabel.Text = "Press M or B Button To Ready Up";
+            mainPictureBox.BackgroundImage = Properties.Resources.mainLobby;
+            mainPictureBox2.Visible = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M)       //red button press
+            {
+                if (scene == 0) { scene = 1; }
+                else if (scene == 1) { scene = 5; }
+                else if (scene == 2) { scene = 4; }
+                else if (scene == 3) { scene = 4; }
+                else if (scene == 4) { scene = 0; }
+                else if (scene == 5) {  }
+                else if (scene == 6) {  }
+                else if (scene == 7) { }
+                else if (scene == 8) {  }
+                else if (scene == 9) { }
+                else if (scene == 10) {  }
+                else if (scene == 11) {  }
+                else if (scene == 12) {  }
+                else if (scene == 13) {  }
+                else if (scene == 14) {  }
+                else if (scene == 15) {  }
+                else if (scene == 16) {  }
+                else if (scene == 17) {  }
+                else if (scene == 18) {  }
+                else if (scene == 19) {  }
+                else if (scene == 20) { }
+                else if (scene == 21) {  }
+
+            }
+            else if (e.KeyCode == Keys.B)  //blue button press
+            {
+                if (scene == 0) { scene = 1; }
+            }
+            else if (scene == 1) { scene = 2; }
+            else if (scene == 2) { scene = 3; }
+            else if (scene == 3) { scene = 4; }
+            else if (scene == 4) { scene = 0; }
+            else if (scene == 5) {  }
+            else if (scene == 6) {  }
+            else if (scene == 7) {  }
+            else if (scene == 8) {  }
+            else if (scene == 9) {  }
+            else if (scene == 10) {  }
+            else if (scene == 11) {  }
+            else if (scene == 12) { }
+            else if (scene == 13) {  }
+            else if (scene == 14) { }
+            else if (scene == 15) {  }
+            else if (scene == 16) {  }
+            else if (scene == 17) {  }
+            else if (scene == 18) {  }
+            else if (scene == 19) { }
+            else if (scene == 20) {  }
+            else if (scene == 21) { }
+
+            switch (scene)
+            {
+                case 0:  //start scene  
+             
+                    break;
+                case 1:
+                    imageBox.BackgroundImage = Properties.Resources.greenButton;
+                    imageBox1.BackgroundImage = Properties.Resources.redButton;
+                    mainPictureBox2.BackgroundImage = Properties.Resources.seasonFiveMap;
+                    mainPictureBox.Visible = false;
+                    mainPictureBox2.Visible = true;
+                    topButtonLabel.Text = "Salty Towers";
+                    bottomButtonLabel.Text = "Retail Row";
+                    titleLabel.Text = "You are in the bus. Where are you dropping?";
+                    break;
+                case 2:
+                    imageBox.BackgroundImage = Properties.Resources.greenButton;
+                    imageBox1.BackgroundImage = Properties.Resources.redButton;
+                    mainPictureBox2.BackgroundImage = Properties.Resources.seasonFiveMap;
+                    mainPictureBox.Visible = false;
+                    mainPictureBox2.Visible = true;
+                    titleLabel.Text = "You decided to Drop Salty Towers where a lot of other people have landed at. What do you do?";
+                    topButtonLabel.Text = "Land at house far away";
+                    bottomButtonLabel.Text = "Rush in";
+                    break;
+                case 3:
+                    titleLabel.Text = "Turns out someone else landed their first and ends up killing you";
+                    break;
+                case 4:
+                    titleLabel.Text = "You've died but Great Try!\nPress M or B To Go Back To the Menu";
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+                    break;
+                case 99:
+
+                    Refresh();
+
+                    Thread.Sleep(2000);
+                    Application.Exit();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
